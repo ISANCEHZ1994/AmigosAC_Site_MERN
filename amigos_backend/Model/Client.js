d7cf7e1f29138796b1e1a805836eed990f4face2
Israel Sanchez <Israel.pro1994@gmail.com>
2021-04-02 15:01:18 -0500

    connected to MongoAtlas

---
+++ amigos_backend/Model/Client.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose';
+
+const ClientSchema = mongoose.Schema({
+    fullName:{
+
+    },
+    email:{
+
+    },
+   
+})
--- amigos_backend/index.js
+++ amigos_backend/index.js
@@ -1,27 +1,27 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
-import routes from '../yugioh_store_backend/routes/store_routes'; // importing the routes from file datingRoutes from folder routes
-import axios from 'axios';
+// import axios from 'axios';
 
 const app = express();
+
 const PORT = process.env.PORT || 3001;
 
 // Mongo Connection - we are now connected to MongoDB and can use it however we like
 mongoose.Promise = global.Promise; 
-mongoose.connect('mongodb://localhost/card-database', {
+mongoose.connect("mongodb+srv://ISANCHEZ94:Is092094@amigossite.36dzf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    
 });
 
-// Body-Parser Setup 
+// Body-Parser via Express Setup 
 app.use(express.json({ extended: false })); 
 
 // CORS (Cross Origin Resource Sharing) Setup
 app.use( cors() ); 
-// this is what makes it ok for frontend to get information from backend
 
-routes(app);
+// routes(app);
 
 // EXPRESS ROUTING ---------------------------------------->
 
